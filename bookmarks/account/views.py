cbe985a1767eb38cf5d05d746ce98b37af13833e
Mario Celis <celioso1@hotmail.com>
2024-07-31 20:59:57 -0500

    Using signals for denormalizing counts

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -35,15 +35,16 @@ def dashboard(request):
     # Display all actions by default
     actions = Action.objects.exclude(user=request.user)
     following_ids = request.user.following.values_list('id',
-                                                    flat=True)
+                                                        flat=True)
     if following_ids:
         # If user is following others, retrieve only their actions
         actions = actions.filter(user_id__in=following_ids)
     actions = actions.select_related('user', 'user__profile')\
-                    .prefetch_related('target')[:10]
+                        .prefetch_related('target')[:10]
     return render(request,
-                'account/dashboard.html',
-                {'section': 'dashboard'})
+                    'account/dashboard.html',
+                    {'section': 'dashboard',
+                    'actions': actions})
 
 def register(request):
     if request.method == 'POST':
