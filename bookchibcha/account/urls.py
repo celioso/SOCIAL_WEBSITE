7b5c3122da68f15579ab81f3294f8e4f2dca5aba
Mario Celis <celioso1@hotmail.com>
2025-11-29 18:15:29 -0500

    Password changed

--- bookchibcha/account/urls.py
+++ bookchibcha/account/urls.py
@@ -1,7 +1,26 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path("login/", views.user_login, name="login")
+    #path("login/", views.user_login, name="login")
+
+    # Login/logout url 
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+
+    # change password urls
+    path("password-change/", 
+        auth_views.PasswordChangeView.as_view(), 
+        name="password_change"),
+
+    path("password-change/done/", 
+        auth_views.PasswordChangeDoneView.as_view(), 
+        name="password_change_done"),
+
+    # reset password urls 166=193
+
+
+    path("", views.dashboard, name="dashboard"),
 ]
 
