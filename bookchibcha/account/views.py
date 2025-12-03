103b8508db5af53fd0e02123884fe5403ccdc390
Mario Celis <celioso1@hotmail.com>
2025-12-02 21:01:18 -0500

    Edit your account

--- bookchibcha/account/views.py
+++ bookchibcha/account/views.py
@@ -50,4 +50,26 @@ def register(request):
                     "account/register.html",
                     {"user_form": user_form})
 
-#@login_required
+@login_required
+def edit(request):
+    if request.method == "POST":
+        user_form = UserEditForm(instance=request.user,
+                                data=request.POST)
+        profile_form = ProfileEditForm(
+                                    instance=request.user.profile,
+                                    data=request.POST,
+                                    files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form=ProfileEditForm(
+                                    instance=request.user.profile)
+    
+    return render(request,
+                "account/edit.html",
+                {"user_form":user_form,
+                "profile_form": profile_form})
+
+#190-217
