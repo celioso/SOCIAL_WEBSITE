8ea22b0d2a22e76185b71916b56064d850e850f9
Mario Celis <celioso1@hotmail.com>
2025-12-12 20:45:14 -0500

    Registering the image model in the administration site

--- bookchibcha/bookchibcha/settings.py
+++ bookchibcha/bookchibcha/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "social_django",
     "django_extensions",
+    "images.apps.ImagesConfig",
 ]
 
 MIDDLEWARE = [
@@ -179,3 +180,16 @@ SOCIAL_AUTH_TWITTER_SECRET= config('API_Key_Secret')
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = config('GOOGLE_OAUTH2_KEY') # Google Client ID
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config('GOOGLE_OAUTH2_SECRET') # Google Client Secret
+
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+]
